2ccce7799e2ff3e7d2ab4925f8de7e097c1c4b48
admotaleb <59257782+admotaleb@users.noreply.github.com>
2024-02-14 12:48:52 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
-import React, { Component } from 'react';
-import LoginPage  from './LoginPage';
-import HomePage from './HomePage';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'
 
- class Conditional_rendering extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       isLoggedIn:true
-    }
-  }
-  
-  render() {
-    if(this.state.isLoggedIn){
-      return (
-        <div>
-          {this.state.isLoggedIn? LoginPage : HomePage}
-          <LoginPage/>
-        </div>
-      )
-    }
-    else{
-      return (
-        <div>
-         <HomePage/>
-        </div>
-      )
-    }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
+   <App/>
     
-  }
-}
-export default Conditional_rendering
+    
+);
+
